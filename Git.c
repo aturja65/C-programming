408f7ebb2d0fb70f7e0f4736eb978711554ed276
Al Jaziz Turja <turjo654@gmail.com>
2021-08-30 16:59:35 +0600

    Twin Prime in C

--- Git.c
+++ Git.c
@@ -2,35 +2,33 @@
 
 int checker(int n)
 {
-   int i;
-   if(n<=1)
-   {
-      return 0;
-   }
-   else
-   {
-       for(i=2;i<n;i++)
-       {
-           if(n%i==0)
-           {
-              return 0;
-           }
-       }
-       return i;
-   }
+    if(n<=1)
+    {
+        return 0;
+    }
+    else
+    {
+        for(int i=2;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
 }
-
 int main()
 {
-   int n,i;
-   printf("Enter a number: ");
-   scanf("%d", &n);
-   for(i=2;i<=n/2;i++)
-   {
-       if(n==checker(i)+checker(n-i))
-       {
-           printf("%d+%d\n", i, n-i);
-       }
-   }
-   return 0;
+    int start,end,i;
+    printf("Enter starting and ending number: ");
+    scanf("%d %d", &start, &end);
+    for(i=start;i<end;i++)
+    {
+        if(checker(i) && checker(i+2))
+        {
+            printf("(%d,%d)", i, i+2);
+        }
+    }
+    return 0;
 }
